482ac4b945c7e30469756b303c881b7181fea685
dawoodjee <adam.daveed@gmail.com>
2024-07-27 12:43:27 +0000

    moved files to new module volunteers renamed volunteers report added groupby to VR changed activity in VL to Task from project

--- malaria_partners/malaria_partners/report/volunteer_report/volunteer_report.js
+++ malaria_partners/malaria_partners/report/volunteer_report/volunteer_report.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2024, Fabric and contributors
 // For license information, please see license.txt
 
-frappe.query_reports["Total Volunteer Hours"] = {
+frappe.query_reports["Volunteer Report"] = {
 	"filters": [
 		{
 			"fieldname": "volunteer",
---
+++ malaria_partners/volunteer_management/doctype/club_type/club_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Fabric and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Club Type", {
+// 	refresh(frm) {
+
+// 	},
+// });

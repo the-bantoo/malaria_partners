ccaaab51d5d45ad8137e6ef558fbc5d4a7c39b61
dawoodjee <adam.dawoodjee@gmail.com>
2025-01-17 13:27:52 +0000

    fix permissions: ws

--- malaria_partners/transport/doctype/fleet_settings/fleet_settings.js
+++ malaria_partners/transport/doctype/fleet_settings/fleet_settings.js
@@ -1,8 +1,15 @@
 // Copyright (c) 2025, Fabric and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Fleet Settings", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Fleet Settings", {
+	refresh(frm) {
+        let workspace = 'Transport';
+            
+        frappe.breadcrumbs.all[frappe.get_route_str()] = {
+            workspace: workspace,
+            doctype: frm.doctype,
+            type: 'Form'
+        };
+        frappe.breadcrumbs.update();
+	},
+});
